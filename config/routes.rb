24540f7233fb5722f2075ee70f29b38a502e2fe6
vagrant <leamerluck@gmail.com>
2015-08-07 04:18:00 +0000

    Added main page and about page with responsive design

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 PersonalBlog::Application.routes.draw do
+  get "/:page" => "static#show"
   resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   #root 'home#index'
+  root 'static#portfolio'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
